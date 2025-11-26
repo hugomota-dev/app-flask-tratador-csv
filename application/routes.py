2b8e3a4f7bc9f019a28cde087245f8fb148e6373
hugomota-dev <hugomota.lab@gmail.com>
2025-11-26 14:20:38 -0300

    Add select_columns route to render select_columns.html template

--- application/routes.py
+++ application/routes.py
@@ -4,4 +4,8 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')    
+    return render_template('index.html')  
+
+@app.route('/select_columns')
+def select_columns():
+    return render_template('select_columns.html')
