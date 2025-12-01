bcf5adc86ea0124c6e7e33c0636182b9506ab71a
hugomota-dev <hugomota.lab@gmail.com>
2025-12-01 11:39:16 -0300

    fix: update import path for main blueprint in application initialization

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, instance_relative_config=True )
     
-    from application.routes import main_blueprint    
+    from application.main_routes import main_blueprint    
     
     app.register_blueprint(main_blueprint)
 
