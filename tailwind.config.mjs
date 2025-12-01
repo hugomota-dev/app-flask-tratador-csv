80047dea977dc43a189d678d97006f2a1b79edc3
hugomota-dev <hugomota.lab@gmail.com>
2025-12-01 11:38:22 -0300

    feat: initialize project with Tailwind CSS and PostCSS configuration
    - Add package.json with project metadata and dependencies for Tailwind CSS and PostCSS.
    - Create postcss.config.mjs to configure PostCSS with Tailwind CSS plugin.
    - Create tailwind.config.mjs to set up Tailwind CSS with specified content paths.

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+// tailwind.config.mjs
+export default {
+  content: [
+    "./application/templates/**/*.html",
+    "./application/static/src/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
