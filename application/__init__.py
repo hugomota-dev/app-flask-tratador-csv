d374d543860ebf4298f4f2e42f42f3ea6b31f88b
hugomota-dev <hugomota.lab@gmail.com>
2025-12-03 09:48:29 -0300

    Modification in the variable blueprint

--- application/__init__.py
+++ application/__init__.py
@@ -6,8 +6,8 @@ def create_app():
     
     app.config['SECRET_KEY'] = 'jduAjlkf&2j4FmtGDj3!@#E4k3nD9$z'
 
-    from .routes import main_bp
-    app.register_blueprint(main_bp)
+    from .routes import main_blueprint
+    app.register_blueprint(main_blueprint)
     
 
     return app
--- application/routes.py
+++ application/routes.py
@@ -1,35 +1,14 @@
 import io
 import base64
 import pandas as pd
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, make_response
 
 # Criamos um "grupo" de rotas chamado main
-main_bp = Blueprint("main", __name__)
+main_blueprint = Blueprint("main", __name__)
 
 
-@main_bp.route("/", methods=["GET", "POST"])
+@main_blueprint.route("/", methods=["GET", "POST"])
 def index():
     # Se alguém acessar a página (GET), mostra o formulário
     if request.method == "GET":
         return render_template("index.html")
-    
-    # Método usado para tentar pegar a chave "csv-upload" (atributo name) do formulário
-    file = request.files.get("csv-upload")
-    if not file:
-        flash("Nenhum arquivo enviado.", "error")
-        return render_template("index.html")
-    
-
-   
-
-    # try:
-    #     file_content = file.read()
-    #     buffer = io.StringIO(file_content.decode('utf-8'))
-    #     df = pd.read_csv(buffer, nrows=0, sep=';')
-    #     columns = df.columns.tolist()
-
-    #     csv_base64 = base64.b64encode(file_content).decode('utf-8')
-    #     return render_template('index.html', columns=columns, csv_data=csv_base64)
-    # except Exception as e:
-    #     flash(f'Erro ao processar o arquivo: {str(e)}', 'error')
-    #     return render_template('index.html')
